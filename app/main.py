84302f9a2a8b93b73123a97a7b97b738207c1e8a
Lucas Teodosio <lucasteodosio10@gmail.com>
2026-03-04 15:25:00 -0300

    solution2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from typing import Callable
 from functools import wraps
+from typing import Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs) -> dict:
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
         if key in cache_dict:
             print("Getting from cache")
